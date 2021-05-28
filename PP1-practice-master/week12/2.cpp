f9123d0c086cca0009445716b4d15fd0d1397550
Kanres-GH <ktnrgs1817@gmail.com>
2021-05-29 00:30:28 +0600

    test commit

--- PP1-practice-master/week12/2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-// #include <map>
-using namespace std;
-
-int main() {
-    pair<double, string> p;
-    cin >> p.first >> p.second;
-    cout << p.first << " " << p.second << endl;
-}
